bfd0777be42dc3fc607350565a93e63fa89d3e0c
qr719830 <qr719830@gmail.com>
2016-02-25 23:49:32 -0500

    added couple comments

--- tweetCrawler/src/twitter4j/examples/SaveTweets.java
+++ tweetCrawler/src/twitter4j/examples/SaveTweets.java
@@ -6,9 +6,15 @@ import java.io.*;
 import java.util.List;
 
 public class SaveTweets {
+	//maxCount for each file, but not implemented strict split based on this count
+	//split will only happen after whole tweets of a query are stored 
+	//and total count exceeds maxCount. 
 	private int maxCount;
+	//fileBase should be a folderpath + filenameprefix 
 	private String fileBase;
+	//current file which is being written into.
 	private String currentFile;
+	//counter for when to store a new file.
 	private int nextThreshold;
 	
 	public int getMaxCount() {
@@ -27,14 +33,6 @@ public class SaveTweets {
 		this.fileBase = fileBase;
 	}
 
-	public int getNextThreshold() {
-		return nextThreshold;
-	}
-
-	public void setNextThreshold(int nextThreshold) {
-		this.nextThreshold = nextThreshold;
-	}
-
 	public String getCurrentFile() {
 		return currentFile;
 	}
@@ -42,7 +40,10 @@ public class SaveTweets {
 	public void setCurrentFile(String currentFile) {
 		this.currentFile = currentFile;
 	}
-
+	//contruct function:
+	//inputs: 
+	//   maxCount: ideal maxCount for each singe file
+	//   fileBase: folderpath + file_prefix
 	SaveTweets(int maxCount, String fileBase) {
 		this.maxCount = maxCount;
 		this.fileBase = fileBase;
@@ -50,11 +51,16 @@ public class SaveTweets {
         this.nextThreshold = maxCount;
 	}
 	
+	//Storing function
+	//inputs:
+	//    currentCount: current total count of tweets save for this filebase
+	//    Result: query result to be store at this time. 
 	public void storeQueryResult(int currentCount, QueryResult Result) {
 		FileOutputStream fos = null;
         OutputStreamWriter osw = null;
         BufferedWriter bw = null;
         try {
+        	//open file with append file set to be true
             fos = new FileOutputStream(currentFile,true);
             osw = new OutputStreamWriter(fos, "UTF-8");
             bw = new BufferedWriter(osw);
@@ -68,6 +74,9 @@ public class SaveTweets {
             e.printStackTrace();
             System.out.println("Failed to store tweets: " + e.getMessage());
         } finally {
+        	//decide if a new file is needed by checking if the total count of 
+        	//tweets exceeds the threshold to create a new file.
+        	//if so set currentFile to next filename and increase threshold by maxcount
         	if (Result.getCount() + currentCount >= nextThreshold) {
         		nextThreshold += maxCount;
         		this.currentFile = this.fileBase + "_" + String.valueOf(currentCount+Result.getCount())+".json";
