7112e018686e390a7d9e6379982859921c5cc44e
qr719830 <qr719830@gmail.com>
2016-04-16 22:19:32 -0400

    added feature selection functions

--- twitter-sentiment-analyzer/gopredictors/analyzer.py
+++ twitter-sentiment-analyzer/gopredictors/analyzer.py
@@ -2,6 +2,7 @@ import re
 import csv
 import pprint
 import nltk.classify
+import random
 
 import itertools
 from nltk.collocations import BigramCollocationFinder
@@ -113,15 +114,15 @@ def devideFullSet(list, seed):
 #tokenize tweets
 def tokenize(tweet):
     featureVector = []
-    tknizer = RegexpTokenizer(r'\w+')
-    #words = tweet.split()
-    words = tknizer.tokenize(tweet)
+    words = tweet.split()
     for w in words:
         #replace two or more with two occurrences
         w = replaceTwoOrMore(w)
-        #strip punctuation
-        #w = w.strip('\'"?,.')
-        if len(w) >1 :
+        match_re = re.search('(\w|\')+',w)
+        if match_re == None:
+            continue
+        w = match_re.group()
+        if w !='_':
             featureVector.append(w.lower())
     return featureVector
 
@@ -147,6 +148,7 @@ def preprocess(data):
 #start extract_features
 def extract_features(tweet):
       global FEATURELIST
+      global NGRAMSFLAG
       tweet_words = []
       tweet_words.extend(set(tweet))
       if NGRAMSFLAG == True:
@@ -160,11 +162,113 @@ def extract_features(tweet):
       return features
  #end
 
+def getTotalCount(trainSet):
+    pos_count = 0
+    neg_count = 0
+    neu_count = 0
+    for each in trainSet:
+        if each[1] == '|Positive|':
+            pos_count += 1
+        elif each[1] == '|Negative|':
+            neg_count += 1
+        elif each[1] == '|Neutral|':
+            neu_count += 1
+    print pos_count,neg_count,neu_count
+    return pos_count,neg_count,neu_count
+
+def selectionFunc1(count):
+    if count['|Positive|'] == 0 or count['|Negative|'] == 0:
+        c1 = (count['|Positive|'] + 1) * 1.0 / (count['|Negative|'] + 1)
+    else:
+        c1 = count['|Positive|'] * 1.0 / count['|Negative|']
+    if c1 < 1:
+        c1 = 1 / c1
+
+    if count['|Neutral|'] == 0 or count['|Negative|'] == 0:
+        c2 = (count['|Neutral|'] + 1) * 1.0 / (count['|Negative|'] + 1)
+    else:
+        c2 = count['|Neutral|'] * 1.0 / count['|Negative|']
+    if c2 < 1:
+        c2 = 1 / c2
+
+    if count['|Neutral|'] == 0 or count['|Positive|'] == 0:
+        c3 = (count['|Neutral|'] + 1) * 1.0 / (count['|Positive|'] + 1)
+    else:
+        c3 = count['|Neutral|'] * 1.0 / count['|Positive|']
+    if c3 < 1:
+        c3 = 1 / c3
+
+    m = max(c1, c2, c3)
+
+def selectionFunc2(feature_count,total_pos_count,total_neg_count,total_neu_count):
+    #              pos,neg,neu
+    # not contain
+    # contain
+    totalData = total_pos_count+total_neg_count+total_neu_count
+    pt = (feature_count['|Positive|'] + feature_count['|Negative|'] + feature_count['|Neutral|'])*1.0/totalData
+    p_not_t = 1-pt
+    p_pos = total_pos_count * 1.0 / totalData
+    p_neg = total_neg_count * 1.0 / totalData
+    p_neu = total_neu_count * 1.0 / totalData
+    N = [[0,0],[0,0],[0,0]]
+    E = [[0,0],[0,0],[0,0]]
+    N[0][0] =  total_pos_count - feature_count['|Positive|']
+    N[0][1] = feature_count['|Positive|']
+    N[1][0] = total_neg_count - feature_count['|Negative|']
+    N[1][1] = feature_count['|Negative|']
+    N[2][0] = total_neu_count - feature_count['|Neutral|']
+    N[2][1] = feature_count['|Neutral|']
+    E[0][0] = totalData*p_pos*p_not_t
+    E[0][1] = totalData * p_pos * pt
+    E[1][0] = totalData * p_neg * p_not_t
+    E[1][1] = totalData * p_neg * pt
+    E[2][0] = totalData * p_neu * p_not_t
+    E[2][1] = totalData * p_neu * pt
+    chi = 0
+    for i in range(0,2):
+        for j in range(0,3):
+            chi += (pow(N[j][i] - E[j][i],2))*1.0/E[j][i]
+    return chi
+
+def selectFeatures(trainSet, featureList):
+    global NGRAMSFLAG
+    sortedList = []
+    total_pos_count,total_neg_count,total_neu_count  = getTotalCount(trainSet)
+    int_count = 0
+    for feature in featureList:
+        int_count += 1
+        count = dict([('|Positive|',0),('|Negative|',0),('|Neutral|',0)])
+        for each in trainSet:
+            tweetTokens = each[0]
+            if NGRAMSFLAG == True:
+                bigram_finder = BigramCollocationFinder.from_words(tweetTokens)
+                bigrams = bigram_finder.nbest(BigramAssocMeasures.pmi, n=20)
+            if (feature in tweetTokens) or (NGRAMSFLAG and feature in bigrams):
+                count[each[1]] += 1
+        if count['|Positive|'] + count['|Negative|'] +count['|Neutral|'] == 1:
+            continue
+        #m = selectionFunc1(count)
+        m = selectionFunc2(count,total_pos_count,total_neg_count,total_neu_count)
+        sortedList.append((feature, m))
+        print int_count, feature, m
+    #topN  = 12000
+    sortedList = sorted(sortedList, key=lambda tup: tup[1])
+    sortedList = sortedList[::-1]
+    topN = 0
+    # for i in range(0,len(featureList)):
+    #     if sortedList[i][1] > 2.71:
+    #         topN+=1
+    #     else:
+    #         break
+    #topN = len(featureList)
+    topN = 12000
+    return [sortedList[i][0] for i in range(0,topN)]
 
 
 def NaiveBayes(trainSet, testSet):
     global FEATURELIST
     global NGRAMSFLAG
+    FEATURELIST = []
     tweets= []
     for row in trainSet:
         sentiment = row[1]
@@ -177,6 +281,8 @@ def NaiveBayes(trainSet, testSet):
 
 
     FEATURELIST = list(set(FEATURELIST))
+
+    FEATURELIST = selectFeatures(trainSet, FEATURELIST)
 
     training_set = nltk.classify.util.apply_features(extract_features, trainSet)
     NBClassifier = nltk.NaiveBayesClassifier.train(training_set)
@@ -211,6 +317,7 @@ if __name__=='__main__':
     if CROSSVALIDFLAG:
         data_file = 'data/gop/august/august_full_form.csv'
         cleanTweets = getCleanTweets(data_file)
+        random.shuffle(cleanTweets)
         cross_validation(cleanTweets, NaiveBayes)
 
     else:
