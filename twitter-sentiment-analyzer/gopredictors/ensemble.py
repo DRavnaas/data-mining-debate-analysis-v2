b60b42533f50e5a4335efd7240c7996610f08ad6
qr719830 <qr719830@gmail.com>
2016-04-21 10:47:44 -0400

    ensemble added bigram

--- twitter-sentiment-analyzer/gopredictors/ensemble.py
+++ twitter-sentiment-analyzer/gopredictors/ensemble.py
@@ -14,10 +14,11 @@ from sklearn.feature_selection import SelectFpr
 from sklearn.svm import SVC
 from sklearn.ensemble import VotingClassifier
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-
 from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
 
 #gloable for feature extraction
+NGRAMSFLAG = None
 FEATURELIST = []
 
 #######################################original simple demo functions
@@ -106,8 +107,13 @@ def tokenize(tweet):
 #start extract_features
 def extract_features(tweet):
       global FEATURELIST
+      global NGRAMSFLAG
       tweet_words = []
       tweet_words.extend(set(tweet))
+      if NGRAMSFLAG == True:
+          bigram_finder = BigramCollocationFinder.from_words(tweet)
+          bigrams = bigram_finder.nbest(BigramAssocMeasures.pmi, n=20)
+          tweet_words.extend(set(bigrams))
 
       features = []
       for word in FEATURELIST:
@@ -119,14 +125,31 @@ def extract_features(tweet):
 
 def classifyAlgo(trainSet, testSet):
     global FEATURELIST
+    global NGRAMSFLAG
     FEATURELIST = []
-    tweets= []
+    featureDict = {}
+    tweets = []
     for row in trainSet:
         sentiment = row[1]
         tweetTokens = row[0]
-        FEATURELIST.extend(tweetTokens)
+        for each in tweetTokens:
+            if featureDict.has_key(each):
+                featureDict[each] += 1
+            else:
+                featureDict[each] = 1
+        if NGRAMSFLAG == True:
+            bigram_finder = BigramCollocationFinder.from_words(tweetTokens)
+            bigrams = bigram_finder.nbest(BigramAssocMeasures.pmi, n=20)
+            for each in bigrams:
+                if featureDict.has_key(each):
+                    featureDict[each] += 1
+                else:
+                    featureDict[each] = 1
 
-    FEATURELIST = list(set(FEATURELIST))
+    for key in featureDict:
+        if featureDict[key] > 1:
+            FEATURELIST.append(key)
+
 
     train_data = []
     train_target = []
@@ -136,65 +159,47 @@ def classifyAlgo(trainSet, testSet):
         train_target.append(train_point[1])
 
 
+    #feature_selector = SelectKBest(chi2, k = 8000)
+    feature_selector = SelectFpr(chi2, alpha = 0.1)
+    train_data = feature_selector.fit_transform(train_data, train_target)
 
-    model1  = MultinomialNB()
-    model2 = LogisticRegression()
-    #model2 = SVC(kernel='linear', probability=True )
 
-    model1.fit(train_data, train_target)
-    model2.fit(train_data, train_target)
-    sig = SentimentIntensityAnalyzer()
+    clf1  = MultinomialNB()
+    clf2 = LogisticRegression()
+    clf3 = KNeighborsClassifier()
+    #model3 = SVC(kernel='linear', probability=True )
 
-    # test_data = []
-    # test_target = []
-    # for test_point in testSet:
-    #     test_data.append(extract_features(test_point[0]))
-    #     test_target.append(test_point[1])
-    #
-    # #test_data = feature_selector.transform(test_data)
-    # predict = model.predict(test_data)
+    model = VotingClassifier(estimators=[('lr', clf1), ('mnb', clf2), ('svm', clf3)], voting='soft', weights=[3, 4, 3])
+    model.fit(train_data, train_target)
+
+    test_data = []
+    test_target = []
+    for test_point in testSet:
+        test_data.append(extract_features(test_point[0]))
+        test_target.append(test_point[1])
+
+    test_data = feature_selector.transform(test_data)
+    predict = model.predict(test_data)
 
     total = 0
     correct = 0
-    for test_point in testSet:
-         total += 1
-         testTweet = extract_features(test_point[0])
-         m1_prob = model1.predict_proba([testTweet])[0]
-         m2_prob = model2.predict_proba([testTweet])[0]
-         vad_res = sig.polarity_scores("".join(test_point[0]))
-         if vad_res['neg'] == vad_res['pos'] == 0:
-             vad_prob = [0.5,0.5]
-         else:
-             vad_prob = [vad_res['neg']/(vad_res['neg']+vad_res['pos']), vad_res['pos']/(vad_res['neg']+vad_res['pos'])]
-         m1_weight = 3
-         m2_weight = 2
-         vad_weight = 1
-         combined_prob = [m1_prob[0]*m1_weight+m2_prob[0]*m2_weight+vad_prob[0]*vad_weight,m1_prob[1]*m1_weight+m2_prob[1]*m2_weight+vad_prob[1]*vad_weight ]
-         if combined_prob[0] > combined_prob[1]:
-             pred_class = '|Negative|'
-         else:
-             pred_class = '|Positive|'
-         if str(pred_class).lower() == str(test_point[1]).lower():
-             correct += 1
+    for i in range(0, len(predict)):
+        total += 1
+        if str(predict[i]).lower() == str(test_target[i]).lower():
+            correct += 1
 
-    # for i in range(0, len(predict)):
-    #     total +=1
-    #     if str(predict[i]).lower() == str(test_target[i]).lower():
-    #         correct +=1
-
-    accuracy =  (correct / float(total)) * 100
+    accuracy = (correct / float(total)) * 100
 
     print "Accuracy : %.2f" % accuracy
     print  "(" + str(correct) + "/" + str(total) + ")"
-    return total,correct
-
+    return total, correct
 
 def preprocess(data_file):
     rawTweets = csv.reader(open(data_file, 'rU'))
     tweets = []
     for each in rawTweets:
-        if each[1] =='|Neutral|':
-            continue
+        #if each[1] =='|Neutral|':
+            #continue
         tweets.append(each)
 
     cleanTweets = []
@@ -206,7 +211,61 @@ def preprocess(data_file):
     return cleanTweets
 
 if __name__=='__main__':
+    NGRAMSFLAG = True
     data_file = 'data/gop/august/august_candidates_form.csv'
     cleanTweets = preprocess(data_file)
     random.shuffle(cleanTweets)
     cross_validation(cleanTweets, classifyAlgo)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # total = 0
+    # correct = 0
+    # for test_point in testSet:
+    #     total += 1
+    #     testTweet = extract_features(test_point[0])
+    #     trans_test_tweet = feature_selector.transform([testTweet])
+    #     m1_prob = model1.predict_proba(trans_test_tweet)[0]
+    #     m2_prob = model2.predict_proba(trans_test_tweet)[0]
+    #     vad_res = sig.polarity_scores("".join(test_point[0]))
+    #     if vad_res['neg'] == vad_res['pos'] == 0:
+    #         vad_prob = [0.5, 0.5]
+    #     else:
+    #         vad_prob = [vad_res['neg'] / (vad_res['neg'] + vad_res['pos']),
+    #                     vad_res['pos'] / (vad_res['neg'] + vad_res['pos'])]
+    #     m1_weight = 3
+    #     m2_weight = 5
+    #     vad_weight = 2
+    #     combined_prob = [m1_prob[0] * m1_weight + m2_prob[0] * m2_weight + vad_prob[0] * vad_weight,
+    #                      m1_prob[1] * m1_weight + m2_prob[1] * m2_weight + vad_prob[1] * vad_weight]
+    #     if combined_prob[0] > combined_prob[1]:
+    #         pred_class = '|Negative|'
+    #     else:
+    #         pred_class = '|Positive|'
+    #     if str(pred_class).lower() == str(test_point[1]).lower():
+    #         correct += 1
