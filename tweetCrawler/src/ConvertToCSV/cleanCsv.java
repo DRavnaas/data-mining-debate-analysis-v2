e841355cdb02ec12146b0fb07d46af227d4a9081
DRavnaas <doylerav@gmail.com>
2016-04-23 13:00:31 -0400

    More tweaks so I can clean existing files too

--- tweetCrawler/src/ConvertToCSV/cleanCsv.java
+++ tweetCrawler/src/ConvertToCSV/cleanCsv.java
@@ -3,6 +3,11 @@ package ConvertToCSV;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
 
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
@@ -29,6 +34,14 @@ public class cleanCsv
         File src = new File(args[0]);
         File dest = new File(args[1]);
         String absolutePath = src.getAbsolutePath();
+
+        if (absolutePath.equalsIgnoreCase(dest.getAbsolutePath()))
+        {
+            System.err.println("Source cannot be the same as destination!");
+            return;
+        }
+
+        System.out.println("Reading file at " + absolutePath);
         System.out.println("Reading file at " + absolutePath);
 
         CSVReader reader = null;
@@ -52,42 +65,86 @@ public class cleanCsv
                 System.out.println("Ingesting " + args[0]);
             }
 
-            if (headerFields[0].startsWith("﻿"))
-            {
-                // I think these are byte markers for UTF-8? Strip them off.
-                headerFields[0] = headerFields[0].substring(3);
-            }
-
             // Add a id field to the csv.
-            String[] newHeader = new String[headerFields.length + 1];
-            newHeader[0] = "id";
+            String[] newHeader = new String[headerFields.length];
+
             int textIndex = -1;
+            int tweetIdIndex = 1;
+            int tweetLocationIndex = -1;
+            int tweetUserTimezoneIndex = -1;
+            int idIndex = -1;
+            int candidateIndex = -1;
             boolean idFound = false;
-            int expectedFieldLength = headerFields.length;
+
             for (int i = 0; i < headerFields.length; i++)
             {
-                newHeader[i + 1] = headerFields[i];
+                newHeader[i] = headerFields[i].trim();
 
-                if (headerFields[i].equalsIgnoreCase("text"))
+                if ((newHeader[i].length() > 0) && ((int) newHeader[i].charAt(0) == 65279))
                 {
-                    textIndex = i;
-
+                    newHeader[i] = newHeader[i].substring(1);
                 }
 
-                if (headerFields[i].equalsIgnoreCase("id"))
+                if (newHeader[i].equalsIgnoreCase("id"))
                 {
                     idFound = true;
+                    idIndex = i;
+                    continue;
                 }
+
+                if (newHeader[i].equalsIgnoreCase("text"))
+                {
+                    textIndex = i;
+                    continue;
+                }
+
+                if (newHeader[i].equalsIgnoreCase("candidate") || newHeader[i].equalsIgnoreCase("candiate"))
+                {
+                    candidateIndex = i;
+                    continue;
+                }
+
+                if (newHeader[i].equalsIgnoreCase("tweet_id"))
+                {
+                    tweetIdIndex = i;
+                    continue;
+                }
+
+                if (newHeader[i].equalsIgnoreCase("tweet_location"))
+                {
+                    tweetLocationIndex = i;
+                    continue;
+                }
+
+                if (newHeader[i].equalsIgnoreCase("user_timezone"))
+                {
+                    tweetUserTimezoneIndex = i;
+                    continue;
+                }
+
             }
+
+            // Get the expected input field length (before we insert an id field, which the input
+            // won't have)
+            int expectedFieldLength = headerFields.length;
 
             // if we need to insert an id field, use the new header we built.
             if (!idFound)
             {
-                textIndex = textIndex + 1;
-                // expectedFieldLength = expectedFieldLength + 1;
+                // Add it on the end so we don't have to worry about shifting other columns.
 
-                headerFields = newHeader;
+                String[] headerWithId = new String[newHeader.length + 1];
+                for (int i = 0; i < newHeader.length; i++)
+                {
+                    headerWithId[i] = newHeader[i];
+                }
+
+                newHeader[newHeader.length] = "id";
+                idIndex = newHeader.length;
+
             }
+
+            headerFields = newHeader;
 
             if (textIndex == -1)
             {
@@ -111,50 +168,79 @@ public class cleanCsv
                 }
 
                 // the header got inserted a couple times in the
-                // json to csv code.
-                if (fields[0].equalsIgnoreCase("tweet_id"))
+                // json to csv code. If the first field value is tweet_id, skip this row.
+                if (fields[0].equalsIgnoreCase("tweet_id") || fields[1].equalsIgnoreCase("tweet_id")
+                        || fields[2].equalsIgnoreCase("tweet_id"))
                 {
                     continue;
                 }
 
                 // A couple fields have the string "null" which R doesn't like.
-                if (fields[5].equalsIgnoreCase("null"))
+                if (fields[tweetLocationIndex].equalsIgnoreCase("null"))
                 {
-                    fields[5] = "none";
+                    fields[tweetLocationIndex] = "none";
                 }
-                if (fields[6].equalsIgnoreCase("null"))
+                if (fields[tweetUserTimezoneIndex].equalsIgnoreCase("null"))
                 {
-                    fields[6] = "none";
+                    fields[tweetUserTimezoneIndex] = "none";
                 }
 
-                switch (fields[1])
+                switch (fields[candidateIndex])
                 {
                 case "Trump":
-                    fields[1] = "Donald Trump";
+                case "Donald Trump":
+                    fields[candidateIndex] = "Donald Trump";
                     break;
                 case "Rubio":
-                    fields[1] = "Marco Rubio";
+                case "Marco Rubio":
+                    fields[candidateIndex] = "Marco Rubio";
                     break;
                 case "Cruz":
-                    fields[1] = "Ted Cruz";
+                case "Ted Cruz":
+                    fields[candidateIndex] = "Ted Cruz";
                     break;
                 case "Kasich":
-                    fields[1] = "John Kasich";
+                case "John Kasich":
+                    fields[candidateIndex] = "John Kasich";
                     break;
                 default:
-                    fields[1] = "none";
+                    fields[candidateIndex] = "Unknown";
                     break;
                 }
 
                 if (!idFound)
                 {
+                    // Add the id.
                     String[] newFields = new String[fields.length + 1];
-                    newFields[0] = Integer.toString(id);
 
+                    // Copy existing fields
                     for (int i = 0; i < fields.length; i++)
                     {
-                        newFields[i + 1] = fields[i];
+                        newFields[i] = fields[i];
+                        if (fields[i].equalsIgnoreCase("null"))
+                        {
+                            newFields[i] = "none"; // R has special processing for null we want to
+                                                   // avoid.
+                        }
+                        if (fields[i].equals("neutral"))
+                        {
+                            newFields[i] = "Neutral"; // R is case sensitive, collapse to the Aug
+                                                      // value
+                        }
+                        if (fields[i].equals("negative"))
+                        {
+                            newFields[i] = "Negative"; // R has special processing for null we want
+                                                       // to avoid.
+                        }
+                        if (fields[i].equals("positive"))
+                        {
+                            newFields[i] = "Positive"; // R has special processing for null we want
+                                                       // to avoid.
+                        }
                     }
+
+                    // Add our id.
+                    newFields[idIndex] = Integer.toString(id);
 
                     fields = newFields;
                 }
@@ -162,26 +248,61 @@ public class cleanCsv
                 // A couple cleanups on the text column so we can ingest as a csv in r or excel
                 String text = new String(fields[textIndex]);
 
+                // This is an attempt to catch some columns that split because of a comma and no
+                // paired quotes.
+                if (fields.length > textIndex && headerFields[textIndex + 1] == "")
+                {
+                    text = text + ", " + fields[textIndex + 1];
+                }
+                if (fields.length > textIndex + 1 && headerFields[textIndex + 2] == "")
+                {
+                    text = text + ", " + fields[textIndex + 2];
+                }
+
                 // A couple utf-8 representations of the "this tweet was truncated" character that
                 // might sneak in
-
-                if (text.length() > 140)
+                // if (text.matches("[^\\x00-\\x7F]"))
+                if (!isPureAscii(text))
                 {
-                    int c = (int) text.charAt(139);
-                    int c2 = (int) text.charAt(140);
-
-                    if ((c == 0xFFFD) && (c2 == 0xFFFD))
-                    {
-                        fields[textIndex] = fields[textIndex].substring(0, 138);
-                    } else if (c == 0xFFFD)
-                    {
-                        fields[textIndex] = fields[textIndex].substring(0, 139);
-                    }
+                    System.out.println("Non ascii in text " + text);
                 }
-                fields[textIndex].replace("<U+FFFD>", " ");
-                fields[textIndex].replace("\uFFFD", " ");
-                fields[textIndex].replace("\n", " ");
-                fields[textIndex].replace("\r", " ");
+
+                String newText = "";
+
+                for (int i = 0; i < text.length(); i++)
+                {
+                    char c = text.charAt(i);
+
+                    if ((c == 0xFFFD) && i >= 138)
+                    {
+                        // strip these characters at end of line
+                        continue;
+                    }
+
+                    if ((c == '\n') || (c == '\r'))
+                    {
+                        c = ' ';
+                    }
+
+                    if (c >= 0x80)
+                    {
+                        continue;
+                    }
+
+                    newText = newText + c;
+                }
+
+                // R output sometimes adds this for the '...' bit
+                newText.replace("<U+FFFD>", " ");
+
+                text = newText;
+                if (!isPureAscii(fields[textIndex]))
+                {
+                    System.out.println("New text = " + text);
+                    System.out.println("tweet id " + fields[1]);
+                }
+
+                fields[textIndex] = text;
 
                 writer.writeNext(fields);
             }
@@ -189,12 +310,16 @@ public class cleanCsv
             System.out.println("Done with ingestion of " + args[0] + ", output written to " + args[1]);
 
         }
-        catch (Exception e)
+        catch (
+
+        Exception e)
+
         {
             e.printStackTrace();
             throw e;
         }
         finally
+
         {
             if (reader != null)
             {
@@ -209,4 +334,40 @@ public class cleanCsv
 
     }
 
+    static CharsetEncoder asciiEncoder = Charset.forName("US-ASCII").newEncoder(); // or
+                                                                                   // "ISO-8859-1"
+                                                                                   // for ISO Latin
+                                                                                   // 1
+
+    public static boolean isPureAscii(String v)
+    {
+        return asciiEncoder.canEncode(v);
+    }
+
+    public static String encodeToAscii(String v)
+    {
+
+        String newText = v;
+        try
+        {
+            CharBuffer uCharBuffer = CharBuffer.wrap(v);
+
+            // Encode the remaining content of a single input character buffer to a new byte buffer.
+            // Converts to ISO-8859-1 bytes and stores them to the byte buffer
+
+            ByteBuffer bbuf = asciiEncoder.encode(uCharBuffer);
+
+            newText = bbuf.toString();
+        }
+        catch (CharacterCodingException e)
+        {
+            System.out.println("Character Coding Error: " + e.getMessage());
+        }
+        finally
+        {
+            asciiEncoder.reset();
+        }
+        return newText;
+
+    }
 }
